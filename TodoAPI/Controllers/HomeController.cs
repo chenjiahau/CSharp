a8eed809c25cc1d47594f5a52831fd3e400da05e
ivanchen <ivan.chen@dlinkcorp.com>
2024-07-13 18:25:57 +0800

    Initialize TodoAPI

---
+++ TodoAPI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoAPI.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        [Route("Home")]
+        public IActionResult Index()
+        {
+            return Ok("Home");
+        }
+    }
+}
+
